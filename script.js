1afdb5d53e1a1bcb6ca4c1e138362ed6daa2779b
Shaeen Singh <shaeenkevinsingh@gmail.com>
2021-07-01 17:06:27 +0200

    adding charater and object collision

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ function jump() {
         character.classList.remove("animate");
     }, 500);
 }
+
+var collision = setInterval(function () {
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+
+    if (blockLeft < 20 && blockLeft > 0 && characterTop >= 130) {
+        block.style.animation = "none";
+        block.style.display = "none";
+        alert("You lose, Refresh the page to try again :D")
+    } 
+}, 10);
