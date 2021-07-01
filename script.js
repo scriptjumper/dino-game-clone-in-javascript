c102bad999e3bbd7b7b9eedd9d14e9fda6beccbf
Shaeen Singh <shaeenkevinsingh@gmail.com>
2021-07-01 16:56:46 +0200

    only adding the animation class if it hasn't been added

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ var character = document.getElementById("character");
 var block = document.getElementById("block");
 
 function jump() {
-    character.classList.add("animate");
+    if (character.classList != "animate") {
+        character.classList.add("animate");
+    }
 
     setTimeout(function () {
         character.classList.remove("animate");
